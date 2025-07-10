fe331ad035ef0ccdcd518b1e61d972880c54c974
ricardorub <65383995+ricardorub@users.noreply.github.com>
2025-07-10 00:23:01 -0500

    Delete src directory

--- src/main/java/com/example/Peloteros/dao/CanchaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.peloteros.dao;
-
-import com.example.peloteros.model.Cancha;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CanchaRepository extends JpaRepository<Cancha, Long> {
-}
--- src/main/java/com/example/Peloteros/dao/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.example.peloteros.dao;
-
-import com.example.peloteros.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByEmail(String email);
-}
